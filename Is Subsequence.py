64e5d719c0dac706a3acf447476ae54a4a1f943a
hsinhuibiga <hsinhuibiga@gmail.com>
2020-06-22 17:31:41 +1000

    Add files via upload

---
+++ Is Subsequence.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        queue = collections.deque(s)
+        for c in t:
+            if not queue: return True
+            if c == queue[0]:
+                queue.popleft()
+        return not queue
