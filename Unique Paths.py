96a1d6cc32b89236a2da281047f74e5fd0da32a7
hsinhuibiga <hsinhuibiga@gmail.com>
2020-06-30 00:42:56 +1000

    Add files via upload

---
+++ Unique Paths.py
@@ -0,0 +1,16 @@
+# Unique Paths
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    continue
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        return dp[m - 1][n - 1]
