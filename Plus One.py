5a6c91486a196833d616927a2466163c70770a0b
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-07 00:12:48 +1000

    Add files via upload

---
+++ Plus One.py
@@ -0,0 +1,19 @@
+#Plus One
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        plus = 1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] + plus > 9:
+                digits[i] = 0
+                plus = 1
+            else:
+                digits[i] = digits[i] + plus
+                plus = 0
+        if plus == 1:
+            digits.insert(0, 1)
+        return digits
