9cea84473337b49a418ac40933533267fc7c6511
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-08 00:10:35 +1000

    Add files via upload

---
+++ Island Perimeter.py
@@ -0,0 +1,22 @@
+#Island Perimeter
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        M, N = len(grid), len(grid[0])
+        counts = 0
+        neighbors = 0
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == 1:
+                    counts += 1
+                    if i < M - 1:
+                        if grid[i + 1][j] == 1:
+                            neighbors += 1
+                    if j < N - 1:
+                        if grid[i][j + 1] == 1:
+                            neighbors += 1
+        return 4 * counts - 2 * neighbors
