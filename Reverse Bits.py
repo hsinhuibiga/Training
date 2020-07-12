d3dff581912c4c7514cd3aaa272c099835572cba
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-12 17:58:22 +1000

    Add files via upload

---
+++ Reverse Bits.py
@@ -0,0 +1,9 @@
+#Reverse Bits
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        bins = bin(n)[2:][::-1]
+        rev = (bins + '0' * (32 - len(bins)))
+        return int(rev, 2)
