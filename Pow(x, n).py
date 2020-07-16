7fa1bc9cb2f021f9523726d1fbcc3f1fa8bf047c
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-16 19:39:26 +1000

    Add files via upload

---
+++ Pow(x, n).py
@@ -0,0 +1,20 @@
+#Pow(x, n)
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        if n % 2:
+            return x * self.myPow(x, n - 1)
+        return self.myPow(x * x, n / 2)
+
+
+    
