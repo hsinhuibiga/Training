0d85900d7f022e60be2b1c3fddbf83440c974b74
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-16 19:33:09 +1000

    Add files via upload

---
+++ Reverse Words in a String.py
@@ -0,0 +1,9 @@
+#Reverse Words in a String
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return " ".join(s.split()[::-1]) 
