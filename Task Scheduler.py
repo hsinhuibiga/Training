6fece96bf711e0075d66bfb0f39197bf09e45263
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-01 23:22:31 +1000

    Add files via upload

---
+++ Task Scheduler.py
@@ -0,0 +1,14 @@
+#Task Scheduler
+
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        count = collections.Counter(tasks)
+        most = count.most_common()[0][1]
+        num_most = len([i for i, v in count.items() if v == most])
+        time = (most - 1) * (n + 1) + num_most
+        return max(time, len(tasks))
